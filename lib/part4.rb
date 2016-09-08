f01d1c99cc2ee4f24fd2fc5835d166f649270c11
Cody Soultz <cody-soultz@uiowa.edu>
2016-09-08 23:57:17 +0000

    Still don't have it initiated with object but other than that part four is done

--- lib/part4.rb
+++ lib/part4.rb
@@ -4,7 +4,19 @@ class Class
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval "YOUR CODE HERE, USE %Q FOR MULTILINE STRINGS"
-
+    class_eval %Q"
+      def #{attr_name}=(new_attr_value)
+        @#{attr_name}_history ||=Array.new(1)
+        @#{attr_name}_history << new_attr_value
+        @#{attr_name} = new_attr_value
+      end"
   end
+end
+
+class Foo
+  def initialize
+    @myVar=1
+  end
+  attr_accessor :myVar
+  attr_accessor_with_history :bar
 end
