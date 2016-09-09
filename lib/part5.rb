d9e0c13a366c99f3133577adc5a8d868c9f32749
Cody Soultz <cody-soultz@uiowa.edu>
2016-09-09 23:08:19 +0000

    Finished part 5 (Should be last commit if no further formating for turning in is needed

--- lib/part5.rb
+++ lib/part5.rb
@@ -2,5 +2,19 @@ class CartesianProduct
   include Enumerable
 
   # YOUR CODE HERE
-
+  attr_accessor :collection1
+  attr_accessor :collection2
+  
+  def initialize(collection1,collection2)
+    @collection1=collection1
+    @collection2=collection2
+  end
+  
+  def each
+    @collection1.each do |item1|
+      @collection2.each do |item2|
+        yield [item1, item2]
+      end
+    end
+  end
 end
