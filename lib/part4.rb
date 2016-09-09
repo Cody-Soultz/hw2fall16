a2884b35edfc0c6cde12cb4dec08f63858d23c01
Cody Soultz <cody-soultz@uiowa.edu>
2016-09-09 22:05:07 +0000

    able to handle one attr_acessor_with_history for now

--- lib/part4.rb
+++ lib/part4.rb
@@ -4,7 +4,14 @@ class Class
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
+    puts "I am running in #{self.to_s} for the attr: #{attr_name}"
     class_eval %Q"
+      alias_method :org_initialize, :initialize
+        def initialize(*args)
+          @#{attr_name}_history||=Array.new(1)
+          puts \"ran my init for #{attr_name}\"
+          org_initialize(*args)
+        end
       def #{attr_name}=(new_attr_value)
         @#{attr_name}_history ||=Array.new(1)
         @#{attr_name}_history << new_attr_value
@@ -16,7 +23,9 @@ end
 class Foo
   def initialize
     @myVar=1
+    puts "In Foo initialize"
   end
   attr_accessor :myVar
   attr_accessor_with_history :bar
+  #attr_accessor_with_history :quz
 end
