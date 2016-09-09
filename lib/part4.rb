8532398e37b8b3424e1b06be773df561857d8241
Cody Soultz <cody-soultz@uiowa.edu>
2016-09-09 22:13:15 +0000

    Finished Part 4 havnt stripped test code yet

--- lib/part4.rb
+++ lib/part4.rb
@@ -4,14 +4,14 @@ class Class
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    puts "I am running in #{self.to_s} for the attr: #{attr_name}"
     class_eval %Q"
-      alias_method :org_initialize, :initialize
-        def initialize(*args)
-          @#{attr_name}_history||=Array.new(1)
-          puts \"ran my init for #{attr_name}\"
-          org_initialize(*args)
-        end
+      old_initialize = instance_method(:initialize)
+      
+      define_method(:initialize) do
+        old_initialize.bind(self).()
+        @#{attr_name}_history ||=Array.new(1)
+      end
+      
       def #{attr_name}=(new_attr_value)
         @#{attr_name}_history ||=Array.new(1)
         @#{attr_name}_history << new_attr_value
@@ -27,5 +27,5 @@ class Foo
   end
   attr_accessor :myVar
   attr_accessor_with_history :bar
-  #attr_accessor_with_history :quz
+  attr_accessor_with_history :quz
 end
